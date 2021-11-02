424d93cc85e1bfadd9c89417954168295fcb53db
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-02 15:30:55 +0100

    cross icon added

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,15 @@ Vue.config.devtools = true;
 new Vue({
     el: "#app",
     data: {
-        toDoList: [],
+        toDoList: ["ciao"],
         task: ""
     },
     methods: {
         addTaskToDo() {
+            /* if (this.toDoList.trim() === "") {
+                return;
+            }
+ */
             let newTask = this.task;
             this.toDoList.push(newTask);
             this.task = "";
