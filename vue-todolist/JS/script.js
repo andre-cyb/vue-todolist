f6993bebb8ab1c7a694e518e252b7b2fb6d02dac
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-02 18:09:40 +0100

    update

---
+++ vue-todolist/JS/script.js
@@ -0,0 +1,22 @@
+Vue.config.devtools = true;
+
+new Vue({
+    el: "#app",
+    data: {
+        toDoList: [],
+        task: ""
+    },
+    methods: {
+        addTaskToDo() {
+            let newTask = this.task;
+            this.toDoList.push(newTask);
+            this.task = "";
+        },
+        onClickRemoveTask(indexOfTask) {
+            this.toDoList.splice(indexOfTask, 1);
+        }
+    },
+    mounted: {
+
+    }
+});
